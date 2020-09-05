869ee656d35fcb3374e698c5e278097d14bb7a7d
Jonathan Simon <jonathan.simon@meetcircle.com>
2020-09-04 22:18:39 -0700

    repointing module to terraform registry

--- terraform/ec2-keypairs/keypairs.tf
+++ terraform/ec2-keypairs/keypairs.tf
@@ -1,5 +1,5 @@
 module "root_key_pair" {
-  source = "git::https://github.com/terraform-aws-modules/key-pair/aws"
+  source = "terraform-aws-modules/key-pair/aws"
   key_name   = "account_ec2_root_key"
   public_key = "${var.ec2_root_public_key}"
 }
