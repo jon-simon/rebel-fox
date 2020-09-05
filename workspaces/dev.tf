03a9203451a871bcb04f42f5a55b33be12eff59f
Jonathan Simon <jonathan.simon@meetcircle.com>
2020-09-05 12:32:03 -0700

    adiing control_plane and worker workspaces to tfcloud

--- workspaces/dev.tf
+++ workspaces/dev.tf
@@ -13,3 +13,35 @@ module "vpc" {
   aws_account_name  = "jibakurei"
   oauth_token_id    = var.oauth_token_id
 }
+
+module "control_plane" {
+  source            = "app.terraform.io/jibakurei/workspace/tfe"
+  workspace_name    = "control-plane"
+  name              = "jibakurei"
+  environment       = "dev"
+  operator          = "circle"
+  org               = "jibakurei"
+  working_directory = "/terraform/control-plane"
+  github_repo       = "meetcircle/rebel-fox"
+  branch            = "master"
+  aws_access_key    = var.aws_access_key_id
+  aws_secret_key    = var.aws_secret_access_key
+  aws_account_name  = "jibakurei"
+  oauth_token_id    = var.oauth_token_id
+}
+
+module "worker" {
+  source            = "app.terraform.io/jibakurei/workspace/tfe"
+  workspace_name    = "worker"
+  name              = "jibakurei"
+  environment       = "dev"
+  operator          = "circle"
+  org               = "jibakurei"
+  working_directory = "/terraform/worker"
+  github_repo       = "meetcircle/rebel-fox"
+  branch            = "master"
+  aws_access_key    = var.aws_access_key_id
+  aws_secret_key    = var.aws_secret_access_key
+  aws_account_name  = "jibakurei"
+  oauth_token_id    = var.oauth_token_id
+}
