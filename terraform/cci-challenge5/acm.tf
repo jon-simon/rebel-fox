5e89ebaecc35d7d04927034b16d178762aaf24b2
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-09-21 22:02:44 -0500

    explicitely setting wait_for_validation on acm cert

--- terraform/cci-challenge5/acm.tf
+++ terraform/cci-challenge5/acm.tf
@@ -5,6 +5,7 @@ module "nlb_cert" {
   domain_name = "challenge5.${data.aws_route53_zone.root.name}"
   zone_id     = data.aws_route53_zone.root.zone_id
 
+  wait_for_validation = true
 
   tags = {
     Name = "challenge5.${data.aws_route53_zone.root.name}"
