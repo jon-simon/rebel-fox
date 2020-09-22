c78aaed56b8cba7150a2358a595e5a6ea4473189
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-09-21 22:45:20 -0500

    adding the asg back in

--- terraform/cci-challenge5/asg.tf
+++ terraform/cci-challenge5/asg.tf
@@ -1,39 +1,39 @@
-# module "asg" {
-#   source  = "terraform-aws-modules/autoscaling/aws"
-#   version = "~> 3.0"
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 3.0"
 
-#   name = "cci_challenge5"
+  name = "cci_challenge5"
 
-#   # Launch configuration
-#   lc_name = "cci-challenge5-lc"
+  # Launch configuration
+  lc_name = "cci-challenge5-lc"
 
-#   image_id                    = data.aws_ami.base.id
-#   instance_type               = var.instance_type
-#   associate_public_ip_address = "false"
-#   iam_instance_profile        = aws_iam_instance_profile.cci_challenge5_instance_profile.id
-#   security_groups             = [module.cci_server_sg.this_security_group_id]
-#   key_name                    = "account_ec2_root_key"
-#   user_data = templatefile("${path.module}/userdata.tmpl", {
-#     tmpl_operator         = local.operator,
-#     tmpl_environment      = local.environment,
-#     tmpl_aws_account_name = local.aws_account_name
-#   })
+  image_id                    = data.aws_ami.base.id
+  instance_type               = var.instance_type
+  associate_public_ip_address = "false"
+  iam_instance_profile        = aws_iam_instance_profile.cci_challenge5_instance_profile.id
+  security_groups             = [module.cci_server_sg.this_security_group_id]
+  key_name                    = "account_ec2_root_key"
+  user_data = templatefile("${path.module}/userdata.tmpl", {
+    tmpl_operator         = local.operator,
+    tmpl_environment      = local.environment,
+    tmpl_aws_account_name = local.aws_account_name
+  })
 
-#   root_block_device = [
-#     {
-#       volume_size = "8"
-#       volume_type = "gp2"
-#     },
-#   ]
+  root_block_device = [
+    {
+      volume_size = "8"
+      volume_type = "gp2"
+    },
+  ]
 
-#   # Auto scaling group
-#   asg_name                  = "cci-challenge5-asg"
-#   vpc_zone_identifier       = data.aws_subnet_ids.private.ids
-#   target_group_arns         = [join(",", module.cci_challenge5_nlb.target_group_arns)]
-#   health_check_type         = "EC2"
-#   min_size                  = 1
-#   max_size                  = 1
-#   desired_capacity          = 1
-#   wait_for_capacity_timeout = 0
+  # Auto scaling group
+  asg_name                  = "cci-challenge5-asg"
+  vpc_zone_identifier       = data.aws_subnet_ids.private.ids
+  target_group_arns         = [join(",", module.cci_challenge5_nlb.target_group_arns)]
+  health_check_type         = "EC2"
+  min_size                  = 1
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
 
-# }
+}
