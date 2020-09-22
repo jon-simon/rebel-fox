376fbdf2614f3ef1ce2e80f14d72117e14b80de3
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-09-21 22:36:39 -0500

    let's get this thing going

--- terraform/cci-challenge5/nlb.tf
+++ terraform/cci-challenge5/nlb.tf
@@ -1,30 +1,30 @@
-# module "cci_challenge5_nlb" {
-#   source  = "terraform-aws-modules/alb/aws"
-#   version = "~> 5.0"
+module "cci_challenge5_nlb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.0"
 
-#   name = "cci-challenge5-nlb"
+  name = "cci-challenge5-nlb"
 
-#   load_balancer_type = "network"
+  load_balancer_type = "network"
 
-#   vpc_id  = data.aws_vpc.jibakurei.id
-#   subnets = data.aws_subnet_ids.public.ids
+  vpc_id  = data.aws_vpc.jibakurei.id
+  subnets = data.aws_subnet_ids.public.ids
 
-#   target_groups = [
-#     {
-#       name_prefix      = "cci-"
-#       backend_protocol = "TCP"
-#       backend_port     = 80
-#       target_type      = "instance"
-#     }
-#   ]
+  target_groups = [
+    {
+      name_prefix      = "cci-"
+      backend_protocol = "TCP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
 
-#   https_listeners = [
-#     {
-#       port               = 443
-#       protocol           = "TLS"
-#       certificate_arn    = module.nlb_cert.this_acm_certificate_arn
-#       target_group_index = 0
-#     }
-#   ]
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "TLS"
+      certificate_arn    = "	arn:aws:acm:us-west-2:552172703831:certificate/b3da632b-f5bc-41e1-8a7f-1c8a33afbf6a"
+      target_group_index = 0
+    }
+  ]
 
-# }
+}
