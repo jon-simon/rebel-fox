0821fd997af18ceb1cee86ed268abe5450d36a96
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-11-12 12:38:19 -0800

    adding poc-vpc resources

---
+++ terraform/poc-vpc/vpc.tf
@@ -0,0 +1,31 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "poc"
+  cidr = "172.31.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["172.31.1.0/24", "172.31.2.0/24", "172.31.3.0/24"]
+  private_subnets  = ["172.31.11.0/24", "172.31.12.0/24", "172.31.13.0/24"]
+
+  tags = "${merge(local.common_tags,
+    {
+      "Name"   = local.service_name,
+      "Module" = "vpc",
+    }
+  )}"
+
+  public_subnet_tags = "${merge(local.common_tags,
+    {
+      "Name"   = "${local.service_name}-pub-subnet",
+      "Module" = "vpc",
+    }
+  )}"
+
+  private_subnet_tags = "${merge(local.common_tags,
+    {
+      "Name"   = "${local.service_name}-priv-subnet",
+      "Module" = "vpc",
+    }
+  )}"
+}
