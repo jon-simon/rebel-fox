8384a95bf62c37e3a55b4844222316d2ec5f0113
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-11-14 22:54:54 -0800

    updating the security group

--- terraform/poc-webservers/security_groups.tf
+++ terraform/poc-webservers/security_groups.tf
@@ -1,11 +1,22 @@
 module "webserver_alb_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+  source = "terraform-aws-modules/security-group/aws"
 
   name        = "webserver"
-  description = "Security group for webserver with HTTP ports open within VPC"
+  description = "Security group for EC2 to allow HTTP from ALB"
   vpc_id      = data.aws_vpc.jibakurei.id
 
-  ingress_cidr_blocks = ["192.168.1.0/24"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = [
+    "all-all"
+  ]
+
+  ingress_with_self = [
+    {
+      rule = "all-all"
+    },
+  ]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["http-80-tcp"]
 }
 
 
