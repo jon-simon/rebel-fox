093bd8b83f8a76ac1b4c2276fd1249261d966f7f
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-11-14 21:32:40 -0800

    removing https listener from alb

--- terraform/poc-webservers/alb.tf
+++ terraform/poc-webservers/alb.tf
@@ -8,7 +8,7 @@ module "alb" {
 
   vpc_id             = data.aws_vpc.jibakurei.id
   subnets            = data.aws_subnet_ids.public.ids
-  security_groups    = [module.web_server_alb_sg.this_security_group_id]
+  security_groups    = [module.webserver_alb_sg.this_security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
@@ -20,15 +20,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-    }
-  ]
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      target_group_index = 0
     }
   ]
 
