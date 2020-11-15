7468bd41fca50ef88e2a732d9070f4bfbb2c788a
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-11-14 21:57:21 -0800

    removing the access logs from alb

--- terraform/poc-webservers/alb.tf
+++ terraform/poc-webservers/alb.tf
@@ -10,13 +10,9 @@ module "alb" {
   subnets            = data.aws_subnet_ids.public.ids
   security_groups    = [module.webserver_alb_sg.this_security_group_id]
 
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
   target_groups = [
     {
-      name_prefix      = "pref-"
+      name_prefix      = "webserv-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
