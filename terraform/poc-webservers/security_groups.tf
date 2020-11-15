4825cc65c24ba14d600caf6a50acc55145f164b6
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2020-11-14 21:24:05 -0800

    Adding ec2 security group

--- terraform/poc-webservers/security_groups.tf
+++ terraform/poc-webservers/security_groups.tf
@@ -1,9 +1,32 @@
-module "web_server_alb_sg" {
+module "webserver_alb_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
+  name        = "webserver"
+  description = "Security group for webserver with HTTP ports open within VPC"
   vpc_id      = data.aws_vpc.jibakurei.id
 
   ingress_cidr_blocks = ["192.168.1.0/24"]
+}
+
+
+module "webserver_ec2_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "webserver"
+  description = "Security group for EC2 to allow HTTP from ALB"
+  vpc_id      = data.aws_vpc.jibakurei.id
+
+computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-tcp"
+      source_security_group_id = module.webserver_alb_sg.this_security_group_id
+    },
+    {
+      from_port                = 80
+      to_port                  = 80
+      protocol                 = "TCP"
+      description              = "Webserver"
+      source_security_group_id = module.webserver_alb_sg.this_security_group_id
+    },
+  ]
 }
