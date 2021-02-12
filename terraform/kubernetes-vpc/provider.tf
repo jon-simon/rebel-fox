20ccfccb85f64111d21ae0e46aea10e38f1a2a45
Jonathan Simon <jonathan.ryan.simon@gmail.com>
2021-02-11 16:03:51 -0800

    upping the provider to version 3

--- terraform/kubernetes-vpc/provider.tf
+++ terraform/kubernetes-vpc/provider.tf
@@ -10,7 +10,7 @@ terraform {
 
 // Defining the AWS provider
 provider "aws" {
-  version    = "~> 2.0"
+  version    = "~> 3.0"
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
